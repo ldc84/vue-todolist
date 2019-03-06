2909982d6f57ac8f672af926de2549e51b508e7c
이동춘 <ldc84@naver.com>
2019-03-06 16:02:43 +0900

    초기 세팅중

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// plugin
+import VueMDCAdapter from 'vue-mdc-adapter'
+import 'vue-mdc-adapter/dist/vue-mdc-adapter.css';
+
+Vue.use(VueMDCAdapter)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <mdc-button raised @click="showSnackbar">Show Snackbar</mdc-button>
+    <mdc-snackbar ref="snackbar"/>
+
+    <mdc-tab-bar>
+      <mdc-tab>item one</mdc-tab>
+      <mdc-tab>item two</mdc-tab>
+      <mdc-tab>item three</mdc-tab>
+    </mdc-tab-bar>
+    
   </div>
 </template>
 
@@ -11,8 +21,26 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  data(){
+    return {
+      
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    showSnackbar() {
+      this.$refs.snackbar.show({ 
+        message: 'Message' 
+      })
+    },
   }
 }
 </script>
+
+
+<style lang="scss">
+/* theme.scss */
+
+</style>
