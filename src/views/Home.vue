ad96a0ccd24af9b7746202f821a478cef836e2e0
ldc84 <ldc84@naver.com>
2019-03-10 02:49:42 +0900

    와꾸 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,20 +6,20 @@
       <mdc-toolbar slot="toolbar">
         <mdc-toolbar-row>
           <mdc-toolbar-section align-start >
-            <mdc-toolbar-menu-icon event="toggle-drawer"></mdc-toolbar-menu-icon>
+            <!-- <mdc-toolbar-menu-icon event="toggle-drawer"></mdc-toolbar-menu-icon> -->
             <mdc-toolbar-title>TO-DO LIST</mdc-toolbar-title>
           </mdc-toolbar-section>
         </mdc-toolbar-row>
         <!-- <mdc-display>TO-DO LIST</mdc-display> -->
       </mdc-toolbar>
       
-      <mdc-drawer slot="drawer" toggle-on="toggle-drawer">
+      <!-- <mdc-drawer slot="drawer" toggle-on="toggle-drawer">
         <mdc-drawer-list>
             <mdc-drawer-item start-icon="inbox">Inbox</mdc-drawer-item>
             <mdc-drawer-item start-icon="send">Sent Mail</mdc-drawer-item>
             <mdc-drawer-item start-icon="drafts">Drafts</mdc-drawer-item>
         </mdc-drawer-list>
-      </mdc-drawer>
+      </mdc-drawer> -->
       
       <main>
 
@@ -58,10 +58,8 @@ export default {
 <style lang="scss" scoped>
 /* theme.scss */
 .home {
-  h1 {
-    height:80px;
-    line-height:80px;
-    font-size:50px;
+  .mdc-toolbar__row {
+    padding-left:10px;
     background:url("../assets/logo.png") no-repeat -30px center;
     background-size:auto 60px;
   }
