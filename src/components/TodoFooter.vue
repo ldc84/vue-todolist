90b83a1ab743eca2305ec5ce679df3bef891c16f
이동춘 <ldc84@naver.com>
2019-03-11 14:18:26 +0900

    수정

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="todo-footer">
-    <mdc-button raised @click="setListClear">All Clear</mdc-button>
+    <div class="foot">
+      <!-- <div class="list-chx" v-if="$store.state.list.length">
+        총 {{ $store.state.list.length }}개 항목 중
+        <strong>{{ succece }} 완료</strong>
+      </div> -->
+      <mdc-button raised @click="setListClear">All Clear</mdc-button>
+    </div>
   </div>
 </template>
 
@@ -12,7 +18,23 @@ export default {
       
     }
   },
+  computed: {
+    succece(){
+      return this.checkedLeng();
+    }
+  },
   methods: {
+    checkedLeng() {
+      let num = 0;
+      const result = this.$store.state.list.forEach(element => {
+        if(element.checked == true){
+          num+1;
+        }
+        console.log(num);
+      });
+      return result;
+      console.log(result);
+    },
     ...mapActions(['setListClear'])
   }
 }
