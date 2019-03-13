8e73425065f5f9aad8b7cdeadc2e402842a526f1
이동춘 <ldc84@naver.com>
2019-03-13 09:16:39 +0900

    체크 수정완료

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="todo-footer">
     <div class="foot">
-      <!-- <div class="list-chx" v-if="$store.state.list.length">
-        총 {{ $store.state.list.length }}개 항목 중
-        <strong>{{ succece }} 완료</strong>
-      </div> -->
+      <div class="list-chx">
+        <transition name="fade" appear>
+          <div v-if="this.$store.state.list.length">
+            총 {{ $store.state.list.length }}개 항목 중
+            <strong>{{ success }}</strong> 완료
+          </div>
+        </transition>
+        <transition name="fade" appear>
+          <div v-if="!this.$store.state.list.length">
+            완료된 항목이 없습니다.
+          </div>
+        </transition>
+      </div>
       <mdc-button raised @click="setListClear">All Clear</mdc-button>
     </div>
   </div>
@@ -19,21 +28,19 @@ export default {
     }
   },
   computed: {
-    succece(){
+    success(){
       return this.checkedLeng();
     }
   },
   methods: {
     checkedLeng() {
       let num = 0;
-      const result = this.$store.state.list.forEach(element => {
-        if(element.checked == true){
-          num+1;
-        }
-        console.log(num);
-      });
-      return result;
-      console.log(result);
+      if(this.$store.state.list.length) {
+        this.$store.state.list.filter(item=> {
+          if(item.checked) num = num+1 ;
+        });
+      }
+      return num;
     },
     ...mapActions(['setListClear'])
   }
@@ -42,6 +49,34 @@ export default {
 
 <style lang="scss" scoped>
   .todo-footer {
-    padding:20px;
+    padding:20px 0;
+    .foot {
+      display:flex;
+      justify-content:space-between;
+      align-items: center;
+      width:100%;
+      .list-chx {
+        position:relative;
+        width:50%;
+        font-size:14px;
+        > div {
+          position:absolute;
+          top:50%;
+          left:0;
+          transform: translateY(-50%);
+          line-height:30px;
+          strong {
+            color:#6200ee;
+          }
+        }
+      }
+    }
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.25s ease-out;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
   }
 </style>
